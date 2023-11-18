66dacb0ade5833c015fda6919b0a96e9a6a0b83f
anirudtate <anirud.programming@gmail.com>
2023-11-18 18:59:37 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,150 @@
-import Image from 'next/image'
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const word = "aeiou";
+  const [currentTry, setCurrentTry] = useState(0);
+  const [restarter, setRestarter] = useState(false);
+
+  useEffect(() => {
+    setCurrentTry(0);
+  }, [restarter]);
+
+  const restart = () => {
+    setRestarter((prev) => !prev);
+  };
+
+  const handleSubmit = () => {
+    setCurrentTry((prev) => prev + 1);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="container min-h-screen flex items-center justify-center">
+      <div className="flex flex-col gap-2">
+        {[...Array(5)].map((_, idx) => (
+          <Row
+            key={idx}
+            disabled={idx > currentTry}
+            onSubmit={handleSubmit}
+            active={currentTry === idx}
+            restarter={restarter}
+            word={word}
+          />
+        ))}
+        <div className="p-0" />
+        <div className="flex gap-2">
+          <Button className="flex-1" variant="secondary" onClick={restart}>
+            Restart
+          </Button>
+          <Button className="flex-1" onClick={handleSubmit}>
+            Submit
+          </Button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
+}
+
+function Row({
+  disabled,
+  onSubmit,
+  active,
+  restarter,
+  word,
+}: {
+  disabled: boolean;
+  onSubmit: () => void;
+  active: boolean;
+  restarter: boolean;
+  word: string;
+}) {
+  const [letters, setLetters] = useState(["", "", "", "", ""]);
+  const [focusedLetter, setFocusedLetter] = useState(0);
+  const ref = useRef<Array<HTMLInputElement | null>>([]);
+
+  useEffect(() => {
+    if (active && ref.current?.[focusedLetter]) {
+      ref.current?.[focusedLetter]?.focus();
+    }
+  }, [focusedLetter, active]);
+
+  useEffect(() => {
+    setLetters(["", "", "", "", ""]);
+    setFocusedLetter(0);
+  }, [restarter]);
+
+  const focusNext = () => {
+    if (focusedLetter === 4) return;
+    setFocusedLetter((prev) => prev + 1);
+  };
+
+  const focusPrev = () => {
+    if (focusedLetter === 0) return;
+    setFocusedLetter((prev) => prev - 1);
+  };
+
+  const isInWord = (letter: string) => {
+    return !disabled && !active && word.includes(letter) && letter !== "";
+  };
+
+  const isAtPosition = (letter: string, idx: number) => {
+    if (!disabled && !active) {
+      console.log(letter, idx, word, word[idx] === letter);
+    }
+    return !disabled && !active && word[idx] === letter[0];
+  };
+
+  return (
+    <div className="flex gap-2">
+      {[...Array(5)].map((_, idx) => (
+        <Input
+          key={idx}
+          ref={(el) => (ref.current[idx] = el)}
+          className={
+            "h-10 w-10 text-center " +
+            (isInWord(letters[idx]) ? "border-yellow-500 " : "") +
+            (isAtPosition(letters[idx], idx) ? "border-green-500 " : "")
+          }
+          placeholder={disabled ? "" : "?"}
+          disabled={disabled}
+          value={letters[idx]}
+          onFocus={() => setFocusedLetter(idx)}
+          onKeyDown={(e) => {
+            const input = e.key;
+            if (input === "Enter") {
+              onSubmit();
+            }
+            if (input === "Backspace") {
+              setLetters((prev) => {
+                const newLetters = [...prev];
+                newLetters[idx] = "";
+                return newLetters;
+              });
+              focusPrev();
+            }
+            if (input === "ArrowRight") {
+              focusNext();
+            }
+            if (input === "ArrowLeft") {
+              focusPrev();
+            }
+            if (input.length !== 1) return;
+            let letter = input.charAt(0);
+            if (letter <= "Z" && letter >= "A") letter = letter.toLowerCase();
+            if (letter <= "z" && letter >= "a") {
+              setLetters((prev) => {
+                const newLetters = [...prev];
+                newLetters[idx] = letter;
+                return newLetters;
+              });
+              focusNext();
+            }
+          }}
+          onChange={(e) => e.preventDefault()}
+        />
+      ))}
+    </div>
+  );
 }
